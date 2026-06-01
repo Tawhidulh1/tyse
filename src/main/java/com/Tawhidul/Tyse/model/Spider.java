e6216dad95fa3cb9f2c92aff6d86180c655c9200
Tawhidulh1 <Tawhidulh1@gmail.com>
2026-06-01 09:28:19 -0400

    making progress hahahahaha

--- src/main/java/com/Tawhidul/Tyse/model/Spider.java
+++ src/main/java/com/Tawhidul/Tyse/model/Spider.java
@@ -1,6 +1,7 @@
 package com.Tawhidul.Tyse.model;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,21 +19,37 @@ import org.jsoup.select.Elements;
 public class Spider {
 
   private static final ConcurrentHashMap<String, List<String>> robotsCache = new ConcurrentHashMap<>();
+  private static final ConcurrentHashMap.KeySetView<String, Boolean> urlsCache = ConcurrentHashMap.newKeySet();
 
   // TEMPORARY!!
   private static List<String> urls;
 
   public Spider(String seedUrl) {
     urls = new ArrayList<String>();
-    getUrls(seedUrl, 5);
-    System.out.println(urls);
+    getUrls(seedUrl, 1);
+    System.out.println("Got urls " + urls);
   }
 
   public void getUrls(String url, int runs) {
-    if (runs == 0) {
+    if (runs == 0 || !validateUrl(url)) {
       return;
     }
+
     try {
+      URI uri = new URI(url);
+      String host = uri.getHost();
+
+      if (!urlsCache.contains(host)) {
+        System.out.println("New host url found: " + host);
+        String robotsUrl = host + "/robots.txt";
+        if (!robotsCache.containsKey(robotsUrl)) {
+          System.out.println("New Robots.txt found: " + robotsUrl);
+          robotsCache.put(robotsUrl, new ArrayList<String>());
+          List<String> robotsList = robotsCache.get(robotsUrl);
+          parseRobots(robotsList);
+        }
+      }
+
       Document d = Jsoup.connect(url).get();
       Elements links = d.select("a[href]");
       for (Element link : links) {
@@ -43,6 +60,14 @@ public class Spider {
     } catch (Exception e) {
       System.out.println("Error with URL:" + url + " " + e);
     }
+  }
+
+  public void parseRobots(List<String> list) {
+    System.out.println("Parsed Robots.txt " + list);
+  }
+
+  public boolean validateUrl(String url) {
+    return true;
   }
 
   public void urlHandler(String url) {
