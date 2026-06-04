2628ed3d3070bf77a43d22ee7afa36d60e3934b5
Tawhidulh1 <Tawhidulh1@gmail.com>
2026-06-04 08:40:24 -0400

    updated spider

--- src/main/java/com/Tawhidul/Tyse/model/Spider.java
+++ src/main/java/com/Tawhidul/Tyse/model/Spider.java
@@ -1,12 +1,14 @@
 package com.Tawhidul.Tyse.model;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.StringReader;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,57 +24,85 @@ public class Spider {
 
   public Spider(String seedUrl) {
     urls = new ArrayList<String>();
-    getUrls(seedUrl, 1);
+    getUrls(seedUrl, 2);
     System.out.println("Got urls " + urls);
   }
 
   public void getUrls(String url, int runs) {
-    if (runs == 0 || !validateUrl(url)) {
+    if (runs == 0 || !isValidUrl(url)) {
       return;
     }
 
     try {
+
       URI uri = new URI(url);
       String host = uri.getScheme() + "://" + uri.getHost();
+      System.out.println("Found host: " + host);
+      appendRobotsUrl(host);
 
-      String robotsUrl = host + "/robots.txt";
-      if (!robotsCache.containsKey(robotsUrl)) {
-        System.out.println("New Robots.txt found: " + robotsUrl);
+      Connection connection = Jsoup.connect(url)
+          .ignoreHttpErrors(true);
 
-        robotsCache.put(robotsUrl, new ArrayList<String>());
-        List<String> robotsList = robotsCache.get(robotsUrl);
-
-        Document robotsDoc = Jsoup.connect(robotsUrl).get();
-        BufferedReader reader = new BufferedReader(new StringReader(robotsDoc.wholeText()));
-        String line = reader.readLine();
-        while (line != null) {
-          robotsList.add(line);
-          line = reader.readLine();
-        }
-
-        parseRobots(robotsList);
-      }
-      String fullUrl = uri.normalize().toASCIIString();
-      if (!urlsCache.contains(fullUrl)) {
-
+      connection.execute();
+      if (connection.response().statusCode() != 200) {
+        return;
       }
 
-      Document d = Jsoup.connect(url).get();
+      Document d = connection.get();
+
+      appendUrlToQueue(url);
       Elements links = d.select("a[href]");
       for (Element link : links) {
         String linkAsString = link.attr("href");
         getUrls(linkAsString, runs - 1);
       }
+
     } catch (Exception e) {
-      System.out.println("Error with URL:" + url + " " + e);
+      System.err.println("Error with url: " + url);
+      e.printStackTrace();
     }
   }
 
-  public void parseRobots(List<String> list) {
-    System.out.println("Parsed Robots.txt " + list);
+  public void appendRobotsUrl(String host) throws IOException {
+    if (!robotsCache.containsKey(host)) {
+      robotsCache.put(host, new ArrayList<String>());
+      List<String> robotsList = robotsCache.get(host);
+
+      String robotsUrl = host + "/robots.txt";
+      Connection connection = Jsoup.connect(robotsUrl)
+          .ignoreHttpErrors(true);
+      connection.execute();
+
+      if (connection.response().statusCode() != 200) {
+        return;
+      }
+      Document robotsDoc = connection.get();
+
+      BufferedReader br = new BufferedReader(new StringReader(robotsDoc.wholeText()));
+      String currentLine = br.readLine();
+      while (currentLine != null) {
+        robotsList.add(currentLine);
+        currentLine = br.readLine();
+      }
+    }
   }
 
-  public boolean validateUrl(String url) {
+  public void appendUrlToQueue(String url) {
+    if (!urlsCache.contains(url)) { // TODO implement an actual queue system
+      urls.add(url);
+    }
+  }
+
+  public boolean isValidUrl(String url) {
+    try {
+      URI uri = new URI(url);
+      if (uri.getScheme() == null) {
+        return false;
+      }
+    } catch (Exception e) {
+      System.err.println("Unable to validateUrl: " + url);
+      e.printStackTrace();
+    }
     return true;
   }
 
